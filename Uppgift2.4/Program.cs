20ac6b9ab1e1cf405b95a6981e9aec4beec39d12
tili21 <tili08001@utb.vaxjo.se>
2025-11-06 20:52:24 +0100

    Add project files.

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lön för anställd 1: ");
+            int lön1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lön för anställd 2: ");
+            int lön2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lön för anställd 3: ");
+            int lön3 = int.Parse(Console.ReadLine());
+
+            int medel = (lön1 + lön2 + lön3) / 3;
+
+            Console.WriteLine($"Medellönen är {medel} kr.");
+
+            Console.ReadKey();
+        }
+    }
+}
